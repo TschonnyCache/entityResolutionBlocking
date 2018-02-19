a2cd1cc81d5f75fdb400bfacdfabaa1f9afa0c9c
Jukka <jukkaaf@gmail.com>
2018-02-20 00:50:25 +0200

    Quotes in title.basic.tsv are now ignored and loops stopped once directors and movies are found

--- importIMDB.py
+++ importIMDB.py
@@ -22,15 +22,19 @@ with open('name.basics.tsv') as nameFile:
             # extracting the movies s/he is famous for
             knownFor = knownForTitles.split(",")
             wantedMovies.extend(knownFor) # Collecting the movies from the current director in the to query list
+            if len(wantedDirectors) == 0: # Stop looking for directors once they have all been found
+            	break
 
 with open('title.basics.tsv') as moviesFile:
     next(moviesFile) # skip headings
-    reader = csv.reader(moviesFile, delimiter='\t')
+    reader = csv.reader(moviesFile, delimiter='\t', quoting=csv.QUOTE_NONE) #QUOTE_NONE "Instructs reader to perform no special processing of quote characters"
     for tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres in reader:
         if tconst in wantedMovies:
             entity={'idIMDB': tconst, 'title': originalTitle, 'year': startYear}
             entitiesList.append(entity)
-
+            wantedMovies.remove(tconst)
+            if len(wantedMovies) == 0: # Stop looking for movies once they have all been found
+            	break
 
 for entity in entitiesList:
     print "\n new entity \n"
