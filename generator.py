c06b8f0dcaf8abe811035e965ae887b5351d55f4
Jukka <jukkaaf@gmail.com>
2018-03-06 19:39:12 +0200

    modified generator

--- generator.py
+++ generator.py
@@ -6,17 +6,20 @@ entitiesA = []
 entitiesB = []
 
 for i in range(125):
-	entityA = {str(random.randint(100, 150)): str(random.randint(0, 50)), 
-				str(random.randint(151, 200)): str(random.randint(100, 200)), 
-				str(random.randint(201, 250)): str(random.randint(5000, 15000))}
-	entityB = {str(random.randint(100, 150)): str(random.randint(0, 50)), 
-				str(random.randint(151, 200)): str(random.randint(100, 200)), 
-				str(random.randint(201, 250)): str(random.randint(5000, 15000))}
-	if entityA not in entitiesA and entityB not in entitiesB:
-		entitiesA.append(entityA)
-		entitiesB.append(entityB)
+    entityA = {str(random.randint(100, 125)): str(random.randint(0, 50)), 
+                str(random.randint(175, 200)): str(random.randint(100, 200)), 
+                str(random.randint(201, 225)): str(random.randint(5000, 15000))}
+    entityB = {str(random.randint(100, 125)): str(random.randint(0, 50)), 
+                str(random.randint(175, 200)): str(random.randint(100, 200)), 
+                str(random.randint(201, 225)): str(random.randint(5000, 15000))}
+    if entityA not in entitiesA and entityB not in entitiesB:
+        entitiesA.append(entityA)
+        entitiesB.append(entityB)
+    if i < 15:
+        entitiesA.append(entityB)
+        entitiesB.append(entityA)
 
 with open('entitiesA.json','w') as outfile:
-	json.dump(entitiesA, outfile)
+    json.dump(entitiesA, outfile)
 with open('entitiesB.json','w') as outfile:
-	json.dump(entitiesB, outfile)
+    json.dump(entitiesB, outfile)
